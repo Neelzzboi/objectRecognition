f06ed963afc929531a48146f219901d3fa821532
Neelzzboi <85091341+Neelzzboi@users.noreply.github.com>
2023-03-14 19:18:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    height: 350,
+    width: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takepicture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captureImage" src = "'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version-",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/88McJGbJ6/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
